3a8cbe02436c12496e919420666053b092ed92a4
Simon <Simon.Schaefer@koeln.de>
2012-05-06 22:09:24 +0200

    realy fixed actor

--- actors/admin.py
+++ actors/admin.py
@@ -1,8 +1,5 @@
 from actors.models import Actor, Person
 from django.contrib import admin
 
-class ActorAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("id",)}
-
-admin.site.register(Actor, ActorAdmin)
+admin.site.register(Actor)
 admin.site.register(Person)
--- actors/models.py
+++ actors/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
 class Person(models.Model):
+    GenderChoice = (
+        ('m', 'Male'),
+        ('f', 'Female'),
+        ('o', 'Other')
+    )
+
     firstName = models.CharField(max_length=100)
     lastName  = models.CharField(max_length=100)
     picture   = models.ImageField(upload_to='images/person/portrait/', blank=True, null=True)
-    gender    = models.CharField(max_length=1)
+    gender    = models.CharField(max_length=1, choices=GenderChoice, default='o')
     description = models.CharField(max_length=500, blank=True, null=True)
+    alternativeNames = models.TextField()
 
     def __unicode__(self):
         return self.firstName + " " + self.lastName
 
 class Actor(models.Model):
+    id = models.AutoField(primary_key=True)
     person = models.ForeignKey(Person)
     scenes = models.ManyToManyField('movies.Scene', blank=True, null=True)
     movies = models.ManyToManyField('movies.Movie', blank=True, null=True)
@@ -18,11 +26,10 @@ class Actor(models.Model):
     measurements = models.CharField(max_length=20, blank=True, null=True)
     ethnicity = models.CharField(max_length=20, blank=True, null=True)
     link = models.URLField(blank=True, null=True)
-    slug = models.SlugField(max_length=200)
 
     @models.permalink
     def get_absolute_url(self):
-        return ('detail', (self.slug,))
+        return ('detail', (self.id,))
 
     def __unicode__(self):
         return self.person.firstName + " " + self.person.lastName
